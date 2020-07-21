8b49dc971c19ce9a77d9cc5b7edea27b3d171b89
verbal-noun <kaifahsan420@gmail.com>
2020-07-21 15:18:12 +1000

    Adding dynamic argument functionality

--- src/benchmarker.py
+++ src/benchmarker.py
@@ -4,18 +4,23 @@ import time
 import random
 import statistics
 
+
 def benchmarker(functions, args): 
 
     # Dictionary to hold the runtime of the comparing functions 
     times = {f.__name__: [] for f in functions}
 
-    A = [64, 25, 12, 22, 11, 50, 19, 34, 35, 53, 55, 54]
-
+    # Load the arguments to proper functions 
+    argument_dict = {}
+    for i in range(len(functions)):
+        argument_dict[functions[i].__name__] = args[i]
+    
     for i in range(3000):
         for _ in range(len(functions)):
             func = random.choice(functions)
+            func_args = argument_dict[func.__name__]
             t0 = time.time()
-            func(A.copy())
+            func(*func_args)
             t1 = time.time()
             times[func.__name__].append((t1-t0)*1000)
 
--- tester.py
+++ tester.py
@@ -1,7 +1,13 @@
-from benchmarker import benchmarker
-from src.sorting import *
+from src.benchmarker import benchmarker
+from lib.sorting import *
 
-
+# List the name of functions we want to compare 
 functions = bubbleSort, selectionSort, insertionSort
 
-benchmarker(functions, 0)
+# List the arguments we want to pass into each function 
+# NOTE - the ORDER of the functions above and arguments you put here MUST MATCH! 
+A = [64, 25, 12, 22, 11, 50, 19, 34, 35, 53, 55, 54]
+args = [[A.copy()], [A.copy()], [A.copy()]]
+
+
+benchmarker(functions, args)
