af2b150fbc06fdd1c892c71702b5bb22f5a58a88
verbal-noun <kaifahsan420@gmail.com>
2020-08-04 00:22:21 +1000

    Adding memory usage visuals

--- src/visuals.py
+++ src/visuals.py
@@ -4,21 +4,24 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 def visualizer(time_stats, memory_stats, functions): 
+    
     # Converting to appropriate data 
     func_names = []
     performance = []
     error = []
+    peak_memory = []
 
     for name, number in time_stats.items():
         func_names.append(name)
         performance.append(statistics.mean(number))
         error.append(statistics.stdev(number))
+        peak_memory.append(memory_stats[name])
 
     y_pos = np.arange(len(func_names))
 
-    # Drawing the bar charts 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
+    # Plotting the runtime performance 
+    fig1 = plt.figure(figsize=(10,10))
+    ax1 = fig1.add_subplot()
 
     ax1.barh(y_pos, performance, xerr=error, align='center', 
              color='green', ecolor='black')
@@ -27,6 +30,23 @@ def visualizer(time_stats, memory_stats, functions):
     # Read labels top to bottom 
     ax1.invert_yaxis()
     # Labels 
-    ax1.set_xlabel('Mean Runtime')
+    ax1.set_xlabel('Mean Runtime (ms)')
     ax1.set_title('Runtime Comparison')
+    
+
+
+    # Plotting the memory performance 
+    fig2 = plt.figure(figsize=(10,10))
+    ax2 =  fig2.add_subplot()
+
+    ax2.barh(y_pos, peak_memory, align='center')
+    ax2.set_yticks(y_pos)
+    ax2.set_yticklabels(func_names)
+    #Read labels top to bottom 
+    ax2.invert_yaxis()
+    # Labels 
+    ax2.set_xlabel('Peak Memory Use (KB)')
+    ax2.set_title('Memory Usage Comparison')
+    
+    # Show the plot 
     plt.show()
